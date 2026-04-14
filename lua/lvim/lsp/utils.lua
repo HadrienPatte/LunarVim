668db63376f03209cec1727c882dcdcb7d5b9bbd
Hadrien Patte <hadrien.patte@datadoghq.com>
2026-04-14 16:05:56 +0200

    fix(lsp): replace deprecated codelens.refresh() with codelens.enable()

--- lua/lvim/lsp/utils.lua
+++ lua/lvim/lsp/utils.lua
@@ -133,25 +133,7 @@ function M.setup_codelens_refresh(client, bufnr)
   if not status_ok or not codelens_supported then
     return
   end
-  local group = "lsp_code_lens_refresh"
-  local cl_events = { "BufEnter", "InsertLeave" }
-  local ok, cl_autocmds = pcall(vim.api.nvim_get_autocmds, {
-    group = group,
-    buffer = bufnr,
-    event = cl_events,
-  })
-
-  if ok and #cl_autocmds > 0 then
-    return
-  end
-  vim.api.nvim_create_augroup(group, { clear = false })
-  vim.api.nvim_create_autocmd(cl_events, {
-    group = group,
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.codelens.refresh { bufnr = bufnr }
-    end,
-  })
+  vim.lsp.codelens.enable(true, { bufnr = bufnr })
 end
 
 ---filter passed to vim.lsp.buf.format
